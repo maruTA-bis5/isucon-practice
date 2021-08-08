5ef3e5d42727a3dae1bcfaea295e3155b312071d
Takayuki Maruyama <bis5.wsys@gmail.com>
2021-08-08 22:02:26 +0900

    perf: add descending index to estate.popularity

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -17,7 +17,8 @@ CREATE TABLE isuumo.estate
     door_height INTEGER             NOT NULL,
     door_width  INTEGER             NOT NULL,
     features    VARCHAR(64)         NOT NULL,
-    popularity  INTEGER             NOT NULL
+    popularity  INTEGER             NOT NULL,
+    INDEX (popularity DESC)
 );
 
 CREATE TABLE isuumo.chair
