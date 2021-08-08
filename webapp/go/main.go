3a1c8b2e13a10a4fc36b7c9d7f71fa3dbb57504e
Takayuki Maruyama <bis5.wsys@gmail.com>
2021-08-08 20:38:23 +0900

    chore(initialize): init use dump

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -313,28 +313,28 @@ func main() {
 }
 
 func initialize(c echo.Context) error {
-	sqlDir := filepath.Join("..", "mysql", "db")
-	paths := []string{
-		filepath.Join(sqlDir, "0_Schema.sql"),
-		filepath.Join(sqlDir, "1_DummyEstateData.sql"),
-		filepath.Join(sqlDir, "2_DummyChairData.sql"),
-	}
+	dump := filepath.Join("..", "mysql", "initialized.dump")
+	// paths := []string{
+	// 	filepath.Join(sqlDir, "0_Schema.sql"),
+	// 	filepath.Join(sqlDir, "1_DummyEstateData.sql"),
+	// 	filepath.Join(sqlDir, "2_DummyChairData.sql"),
+	// }
 
-	for _, p := range paths {
-		sqlFile, _ := filepath.Abs(p)
-		cmdStr := fmt.Sprintf("mysql -h %v -u %v -p%v -P %v %v < %v",
-			mySQLConnectionData.Host,
-			mySQLConnectionData.User,
-			mySQLConnectionData.Password,
-			mySQLConnectionData.Port,
-			mySQLConnectionData.DBName,
-			sqlFile,
-		)
-		if err := exec.Command("bash", "-c", cmdStr).Run(); err != nil {
-			c.Logger().Errorf("Initialize script error : %v", err)
-			return c.NoContent(http.StatusInternalServerError)
-		}
+	// for _, p := range paths {
+	sqlFile, _ := filepath.Abs(dump)
+	cmdStr := fmt.Sprintf("mysql -h %v -u %v -p%v -P %v %v < %v",
+		mySQLConnectionData.Host,
+		mySQLConnectionData.User,
+		mySQLConnectionData.Password,
+		mySQLConnectionData.Port,
+		mySQLConnectionData.DBName,
+		sqlFile,
+	)
+	if err := exec.Command("bash", "-c", cmdStr).Run(); err != nil {
+		c.Logger().Errorf("Initialize script error : %v", err)
+		return c.NoContent(http.StatusInternalServerError)
 	}
+	// }
 
 	return c.JSON(http.StatusOK, InitializeResponse{
 		Language: "go",
