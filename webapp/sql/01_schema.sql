7357d9a593bed3244ba0d31e7a79dfbd20c69567
Takayuki Maruyama <bis5.wsys@gmail.com>
2021-08-14 20:36:39 +0900

    add index to reservations table

--- webapp/sql/01_schema.sql
+++ webapp/sql/01_schema.sql
@@ -22,5 +22,7 @@ CREATE TABLE `reservations` (
   `id`          VARCHAR(255) PRIMARY KEY NOT NULL,
   `schedule_id` VARCHAR(255) NOT NULL,
   `user_id`     VARCHAR(255) NOT NULL,
-  `created_at`  DATETIME(6) NOT NULL
+  `created_at`  DATETIME(6) NOT NULL,
+  INDEX (schedule_id),
+  INDEX (schedule_id, user_id)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
