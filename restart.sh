306753c0bbd7152635e24ba8ce324599f3817ae6
Takayuki Maruyama <bis5.wsys@gmail.com>
2021-08-15 12:03:01 +0900

    update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -xe
 
-sudo systemctl restart xsuportal-api-golang xsuportal-web-golang
+sudo systemctl stop envoy
+sudo rm /var/log/envoy/access.log
+sudo systemctl start envoy
+
+sudo systemctl stop xsuportal-api-golang xsuportal-web-golang
+
+cd ~isucon/repo/webapp/golang
+PATH=$PATH:~isucon/local/golang/bin make
+
+sudo systemctl start xsuportal-api-golang xsuportal-web-golang
+sudo systemctl status xsuportal-api-golang xsuportal-web-golang
+
