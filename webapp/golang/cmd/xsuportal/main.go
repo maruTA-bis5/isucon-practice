d0d7d9a6270fc568da92c33e209ead5fbc7eacd1
Takayuki Maruyama <bis5.wsys@gmail.com>
2021-08-15 14:55:16 +0900

    Revert "最大1秒だってば"
    This reverts commit 294f5406ad36d571f64fd22550079586594474ad.

--- webapp/golang/cmd/xsuportal/main.go
+++ webapp/golang/cmd/xsuportal/main.go
@@ -1195,7 +1195,7 @@ func (*AudienceService) ListTeams(e echo.Context) error {
 }
 
 func (*AudienceService) Dashboard(e echo.Context) error {
-	key := strconv.Itoa(int(time.Now().Unix()))
+	key := strconv.Itoa(int(time.Now().Unix() / 1000))
 	cachedLeaderboard, err := rdb.WithContext(e.Request().Context()).Get(e.Request().Context(), key).Result()
 	if cachedLeaderboard != "" {
 		leaderboard := &resourcespb.Leaderboard{}
