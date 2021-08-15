a4f7b0fa3b7a0990449823505a8230865acde42c
Takayuki Maruyama <bis5.wsys@gmail.com>
2021-08-16 07:19:29 +0900

    nr: record db statistics

--- webapp/golang/cmd/xsuportal/main.go
+++ webapp/golang/cmd/xsuportal/main.go
@@ -95,6 +95,19 @@ func main() {
 	}
 
 	db, _ = xsuportal.GetDB()
+	if nrEnabled {
+		go func() {
+			for {
+				stats := db.Stats()
+				nrApp.RecordCustomMetric("DBStats.MaxOpenConnection", float64(stats.MaxOpenConnections))
+				nrApp.RecordCustomMetric("DBStats.Idle", float64(stats.Idle))
+				nrApp.RecordCustomMetric("DBStats.OpenConnections", float64(stats.OpenConnections))
+				nrApp.RecordCustomMetric("DBStats.InUse", float64(stats.InUse))
+				nrApp.RecordCustomMetric("DBStats.WaitCount", float64(stats.WaitCount))
+				time.Sleep(10 * time.Second)
+			}
+		}()
+	}
 
 	db.SetMaxOpenConns(util.GetEnvInt("XSU_DB_MAX_CONN", 20))
 
